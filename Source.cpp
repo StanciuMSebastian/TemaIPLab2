2c5332d8b7e1b593e1bb6d2643758e63142d5731
Stanciu Sebastian <stanciumsebastian@gmail.com>
2022-11-05 17:07:09 +0200

    Update fisier sursa c++

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,20 @@
 #include <iostream.h>
 
+
+void my_print(char* string)
+{
+	if(string == NULL)
+		return;
+
+	printf("Tocmai ati incercat sa printati stringul:\n);
+
+	for(int i = 0; i < strlen(string); i++)
+		printf("%c", string[i]);
+	printf("\n");
+}
+
 int main()
 {
-	printf("Hej Sokoly!");
+	my_print("Hej Sokoly!");
 	return 1;
 }
